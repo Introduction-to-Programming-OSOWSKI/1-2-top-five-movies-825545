6dc8a7d4abb9bae34a8f1564879d5059df73ff48
benthompson-10 <825545@mystma.org>
2022-09-16 14:38:21 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-#WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(a, b, c, d, e):
+    return(a, b, c, d, e)
+topFiveMovies("oceans11","oceans 13", "oceans 12", "the good the bad and the ugly", "tin-tin")
